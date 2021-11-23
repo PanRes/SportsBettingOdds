9958aa8e8e79a1a4dc9392d98654a8744eafec16
Panagiotis Ressos <p.ressos@hotmail.com>
2021-11-23 18:39:04 +0200

    Added spring boot files

---
+++ src/main/java/gr/pr/accepted/sportbettingodds/SportBettingOddsApplication.java
@@ -0,0 +1,13 @@
+package gr.pr.accepted.sportbettingodds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportBettingOddsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportBettingOddsApplication.class, args);
+	}
+
+}
